e52535a6a447b6b518fc1b594027aaf4582e9cc3
AGehlot <andrew.gehlot@gmail.com>
2019-09-01 22:16:12 -0400

    Added Map ADT class

--- Data-structures/DIY-ADTs.rb
+++ Data-structures/DIY-ADTs.rb
@@ -35,3 +35,45 @@ class Queue
     @queue[-1]
   end
 end
+
+class Map
+
+  def initialize
+    @map = []
+  end
+
+  def set(key, value)
+    @map.each do |subarr|
+      if subarr[0] == key
+        subarr[1..-1] = value
+        return
+      end
+    end
+    @map << [key, value]
+  end
+
+  def get(key)
+    @map.each do |subarr|
+      if subarr[0] == key
+        return subarr[1..-1]
+      end
+    end
+  end
+
+  def delete(key)
+    @map.each do |subarr|
+      if subarr[0] == key
+        @map.delete(subarr)
+      end
+    end
+  end
+
+  def show
+    print @map
+  end
+end
+
+
+
+        
+    
