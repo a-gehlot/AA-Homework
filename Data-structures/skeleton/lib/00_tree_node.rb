8e8e2d8c5a05d8be06f5fa8fd6b753b69b46f629
AGehlot <andrew.gehlot@gmail.com>
2019-09-04 02:00:47 -0400

    Start of parent= methods

--- Data-structures/skeleton/lib/00_tree_node.rb
+++ Data-structures/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,21 @@
 class PolyTreeNode
+    attr_accessor :value, :parent, :children
 
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        if node == nil
+            @parent = nil
+        else
+            @parent = node
+            node.children << self unless node.children.include?(self)
+        end
+    end
+        
+
+    
 end
