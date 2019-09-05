1d0e3a13dc3c48ed064472a1548abb89f1c6ae5d
AGehlot <andrew.gehlot@gmail.com>
2019-09-05 17:30:58 -0400

    Init KPF class

--- Data-structures/Knights-traverse/lib/knights-traverse.rb
+++ Data-structures/Knights-traverse/lib/knights-traverse.rb
@@ -1,1 +1,10 @@
 require "/Users/andrewgehlot/Documents/App Academy/Aa-homeworks/Data-structures/PolyNodeTree/lib/00_tree_node.rb"
+
+class KnightPathFinder
+    kpf = KnightPathFinder.new([0, 0])
+
+end
+
+
+kpf.find_path([2, 1]) # => [[0, 0], [2, 1]]
+kpf.find_path([3, 3]) # => [[0, 0], [2, 1], [3, 3]]
