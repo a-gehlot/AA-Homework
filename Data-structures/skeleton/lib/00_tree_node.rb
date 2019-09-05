c951bef96a9bdc36b55dcfc1d95e50c37b4aced4
AGehlot <andrew.gehlot@gmail.com>
2019-09-05 13:53:41 -0400

    Added add, remove children, bfs, dfs for node

--- Data-structures/skeleton/lib/00_tree_node.rb
+++ Data-structures/skeleton/lib/00_tree_node.rb
@@ -8,12 +8,43 @@ class PolyTreeNode
     end
 
     def parent=(node)
+        old_parent = self.parent
         @parent = node
-        unless node.nil? 
-            node.children << self unless node.children.include?(self)
+        unless @parent == nil || old_parent == @parent
+            node.children << self
+            old_parent.children.delete(self) unless old_parent == nil
+        end
+    end
+
+    def add_child(child)
+        child.parent = self
+    end
+
+    def remove_child(child)
+        if self.children.include?(child)
+            child.parent = nil
+        else 
+            raise "passed argument is not a child"
+        end
+    end
+
+    def dfs(descendant)
+        return self if self.value == descendant
+        self.children.each do |child|
+            search_result = child.dfs(descendant)
+            return search_result unless search_result.nil?
+        end
+        nil
+    end
+
+    def bfs(descendant)
+        queue = [self]
+        until queue.empty?
+            el = queue.shift
+            return el if el.value == descendant
+            el.children.each { |child| queue << child }
         end
     end
         
-
-    
 end
+
