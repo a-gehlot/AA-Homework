24bd50a8d7fe26aa95b7e6c29f90f0e2b14e406c
AGehlot <andrew.gehlot@gmail.com>
2019-09-05 17:33:38 -0400

    Fixed init of KPF

--- Data-structures/Knights-traverse/lib/knights-traverse.rb
+++ Data-structures/Knights-traverse/lib/knights-traverse.rb
@@ -1,10 +1,13 @@
 require "/Users/andrewgehlot/Documents/App Academy/Aa-homeworks/Data-structures/PolyNodeTree/lib/00_tree_node.rb"
 
 class KnightPathFinder
-    kpf = KnightPathFinder.new([0, 0])
+
+    def initialize(starting_position)
+        @root_node = starting_position
+    end
 
 end
 
-
+kpf = KnightPathFinder.new([0, 0])
 kpf.find_path([2, 1]) # => [[0, 0], [2, 1]]
 kpf.find_path([3, 3]) # => [[0, 0], [2, 1], [3, 3]]
