d75c1ec75c8333902ea83fd4cf2d3f8bcbeb50e9
AGehlot <andrew.gehlot@gmail.com>
2019-10-04 18:57:24 -0400

    Working on two modules; merging with master

--- Object-oriented-programming/Chess/Board.rb
+++ Object-oriented-programming/Chess/Board.rb
@@ -20,6 +20,45 @@ class Board
 
 end
 
+module Slideable
+    HORIZONTAL_DIRS = [
+        [0,1],
+        [1,0],
+        [0,-1],
+        [-1,0]
+    ]
+    DIAGONAL_DIRS = [
+        [1,1],
+        [-1,1],
+        [1,-1],
+        [-1,-1]
+    ]
+    
+    def horizontal_dirs
+        HORIZONTAL_DIRS
+    end
+
+    def diagonal_dirs 
+        DIAGONAL_DIRS
+    end
+
+    def moves
+        poss_moves = []
+        deltas = move_dirs
+        deltas.each do |coord|
+            poss_moves.concat(grow_unblocked_moves_in_dir(coord[0],coord[1])
+        end
+        poss_moves
+    end
+
+    def grow_unblocked_moves_in_dir(dx, dy)
+        new_pos = [pos[0] + dx, pos[1] + dy]
+end
+
+module Stepable
+
+end
+
 class Piece
 
 end
