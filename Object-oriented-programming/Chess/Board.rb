a19b34c2954f39ecf3f2a8b239fecab9a8a1490c
AGehlot <andrew.gehlot@gmail.com>
2019-10-10 01:37:06 -0400

    Classes for chess pieces and modules

--- Object-oriented-programming/Chess/Board.rb
+++ Object-oriented-programming/Chess/Board.rb
@@ -1,64 +1,145 @@
 class Board
     def initialize
-        @rows = Array.new(8) { Array.new(8) }
-        (0..8).each do |col|
-            (0..1).each { |row| @rows[row][col] = Piece.new }
-            (6..7).each { |row| @rows[row][col] = Piece.new }
-            (2..5).each { |row| @rows[row][col] = nil }
-        end
+        @rows = Array.new(8) { Array.new(8, nil) }
     end
 
-    def move_piece(start_pos, end_pos)
-        i, j = start_pos
-        a, b = end_pos
-        if !@rows[i][j].is_a?(Piece)
-            raise ArgumentError.new("Not a piece")
+    def [](pos)
+        x, y = pos
+        @rows[x][y]
+    end
+
+    def []=(pos, val)
+        x, y = pos
+        @rows[x][y] = val
+    end
+
+    def valid_pos?(pos)
+        if pos.any? { |val| 0 >= val || val >= 7 }
+            return false
         end
-        @rows[a][b] = @rows[i][j]
-        @rows[i][j] = nil
+        true
+    end
+
+end
+
+class Piece
+    def initialize(color, board, pos)
+        @color = color
+        @board = board
+        @pos = pos
+    end
+
+    def to_s
+    end
+
+    def symbol
+        symbol
     end
 
 end
 
 module Slideable
-    HORIZONTAL_DIRS = [
-        [0,1],
-        [1,0],
-        [0,-1],
-        [-1,0]
-    ]
-    DIAGONAL_DIRS = [
-        [1,1],
-        [-1,1],
-        [1,-1],
-        [-1,-1]
-    ]
-    
-    def horizontal_dirs
-        HORIZONTAL_DIRS
-    end
+    HORIZONTAL_DIRS = [[1,0],[0,1],[-1,0],[0,-1]]
+    DIAGONAL_DIRS = [[1,1],[-1,1],[-1,-1],[1,-1]]
 
-    def diagonal_dirs 
-        DIAGONAL_DIRS
-    end
-
-    def moves
-        poss_moves = []
-        deltas = move_dirs
-        deltas.each do |coord|
-            poss_moves.concat(grow_unblocked_moves_in_dir(coord[0],coord[1])
+    def moves 
+        moves = []
+        if horizontal_dirs
+            HORIZONTAL_DIRS.each do |dir|
+                a, b = dir
+                moves << grow_unblocked_moves_in_dir(a, b)
+            end
         end
-        poss_moves
+        if diagonal_dirs
+            DIAGONAL_DIRS.each do |dir|
+                a, b = dir
+                moves << grow_unblocked_moves_in_dir(a, b)
+            end
+        end
+        moves.flatten(1)
+    end
+
+    def horizontal_dirs
+        move_dirs[:horizontal_dirs]
+    end
+
+    def diagonal_dirs
+        move_dirs[:diagonal_dirs]
     end
 
     def grow_unblocked_moves_in_dir(dx, dy)
-        new_pos = [pos[0] + dx, pos[1] + dy]
+        x, y = @pos
+        new_move = [x + dx, y + dy]
+        grown_moves = []
+        while @board.valid_pos?(new_move)
+            new_move = [x + dx, y + dy]
+            unless @board[new_move].nil?
+                if @board[new_move].color == self.color
+                    break
+                else grown_moves << new_move
+                    break
+                end
+            end
+            grown_moves << new_move
+            x += dx
+            y += dy
+        end
+        grown_moves
+    end
 end
 
-module Stepable
+class Rook < Piece
+    include Slideable
+    def symbol
+        @color == :black ? "♜" : "♖"
+    end
 
+    def move_dirs
+        { horizontal_dirs: true, diagonal_dirs: false }
+    end
 end
 
-class Piece
+class Bishop < Piece
+    include Slideable
+    def symbol
+        @color == :black ? "♝" : "♗"
+    end
 
+    def move_dirs
+        { horizontal_dirs: false, diagonal_dirs: true }
+    end
 end
+
+class Queen < Piece
+    include Slideable
+    def symbol
+        @color == black ? "♛" : "♕"
+    end
+
+    def move_dirs
+        { horizontal_dirs: true, diagonal_dirs: true }
+    end
+end
+
+class King < Piece
+    def symbol
+        @color == black ? "♚" : "♔"
+    end
+
+    def move_diffs
+        [[1,0],[-1,0],[0,1],[0,-1],[1,1],[-1,1],[-1,-1],[1,-1]]
+    end
+end
+
+class Knight < Piece
+    def symbol
+        @color == black ? "♞" : "♘"
+    end
+
+    def move_diffs
+        [[2,1],[1,2],[2,-1],[1,-2],[-2,-1],[-1,-2],[-2,1],[-1,2]]
+    end
+end
+
+
+
